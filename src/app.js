fd43a43e7bd4c3a50ce36c661e1773bd68a00c25
Aadithya-J <jlaadithya@gmail.com>
2024-06-01 09:24:54 +0530

    changed directory structure and added static

--- src/app.js
+++ src/app.js
@@ -3,8 +3,6 @@ const path = require('path')
 const app = express();
 const port = 3000;
 
-app.use(express.static(path.join(__dirname, '../public')));
-
 app.get('/', (req, res) => {
     res.send('Hello World!');
 });
