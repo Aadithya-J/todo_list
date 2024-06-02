e4149b69eb32506f66b2f45690ebdfd34fc58552
SanjayKumar <0508sanjaykumar@gmail.com>
2024-06-02 11:39:58 +0530

    add and delete feature added

--- public/script.js
+++ public/script.js
@@ -1,16 +1,32 @@
 let add = document.querySelector(".mainicon");
-add.addEventListener(('click'),(e)=>{
-    let place = document.querySelector(".bottom");
-    place.innerHTML+=`<div class="card">
-    <div class="desc">
-        This is the first contribution
-    </div>
-    <div class="buttonsTab">
-        <img id="remove" src="icons/bin.svg" style="color: wheat;" alt="">
-        <img id ="edit" src="icons/edit.svg" alt="">
-    </div>
-</div>`
-})
 
-let remove = document.querySelector(".bottom")
-console.log("remove")
+function getRandomColor() {
+  const red = Math.floor(Math.random() * 256);
+  const green = Math.floor(Math.random() * 256);
+  const blue = Math.floor(Math.random() * 256);
+  return `rgb(${red}, ${green}, ${blue})`;
+}
+
+
+add.addEventListener('click', (e) => {
+    let place = document.querySelector(".bottom");
+    place.innerHTML += `
+    <div class="card">
+      <div class="desc" style="background-color:${getRandomColor()}; color:black">
+          This is the first contribution
+      </div>
+      <div class="buttonsTab">
+          <img class="remove" src="icons/bin.svg" style="color: wheat;" alt="">
+          <img class="edit" src="icons/edit.svg" alt="">
+      </div>
+    </div>`;
+});
+
+let bottom = document.querySelector(".bottom");
+bottom.addEventListener("click", (e) => {
+  if (e.target.closest('.remove')) 
+  {
+     e.target.closest('.card').remove()
+  }
+    
+});
