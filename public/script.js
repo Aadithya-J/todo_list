9eea9a2d890e9826380d684e3fef33867fbcba32
SanjayKumar <0508sanjaykumar@gmail.com>
2024-06-02 18:58:56 +0530

    added styles and changed format

--- public/script.js
+++ public/script.js
@@ -1,28 +1,19 @@
 let add = document.querySelector(".mainicon");
 
-function getRandomColor() {
-  const red = Math.floor(Math.random() * 256);
-  const green = Math.floor(Math.random() * 256);
-  const blue = Math.floor(Math.random() * 256);
-  return `rgb(${red}, ${green}, ${blue})`;
-}
-
-
 add.addEventListener('click', (e) => {
-    let place = document.querySelector(".bottom");
+    let place = document.querySelector(".left");
     place.innerHTML += `
     <div class="card">
-      <div class="desc" style="background-color:${getRandomColor()}; color:black">
+      <div class="desc" color:black">
           This is the first contribution
       </div>
       <div class="buttonsTab">
           <img class="remove" src="icons/bin.svg" style="color: wheat;" alt="">
-          <img class="edit" src="icons/edit.svg" alt="">
       </div>
     </div>`;
 });
 
-let bottom = document.querySelector(".bottom");
+let bottom = document.querySelector(".left");
 bottom.addEventListener("click", (e) => {
   if (e.target.closest('.remove')) 
   {
