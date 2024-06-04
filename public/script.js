5ce6998fdd50a43ab9c5cc6f15ed8afd3e5c73f0
SanjayKumar <0508sanjaykumar@gmail.com>
2024-06-04 11:12:08 +0530

    fixed popping up

--- public/script.js
+++ public/script.js
@@ -1,21 +1,25 @@
 let add = document.querySelector(".mainicon");
 let submitBtn = document.querySelector("#submitBtn");
-function submit (event)
-{
+
+function submit(event) {
+  event.preventDefault(); // Prevent the default form submission
   const titleInput = document.getElementById("title");
   const title = titleInput.value;
-  const cards = document.querySelectorAll(".desc");
-  const lastCard = cards[cards.length - 1]; 
-  lastCard.innerHTML = `${title}`;
   document.querySelector(".pop-up").style.bottom = "-50%";
+  return title;
 }
+
 add.addEventListener('click', (e) => {
-    let place = document.querySelector(".left");
-    document.querySelector(".pop-up").style.bottom = "50%";
-    place.innerHTML += `
+  document.querySelector(".pop-up").style.bottom = "50%";
+});
+
+submitBtn.addEventListener('click', (e) => {
+  const title = submit(e);
+  let place = document.querySelector(".left");
+  place.innerHTML += `
     <div class="card">
-      <div class="desc" color:black">
-          This is the first contribution
+      <div class="desc" style="color:white">
+          ${title}
       </div>
       <div class="buttonsTab">
           <img class="remove" src="icons/bin.svg" style="color: wheat;" alt="">
@@ -25,9 +29,7 @@ add.addEventListener('click', (e) => {
 
 let bottom = document.querySelector(".left");
 bottom.addEventListener("click", (e) => {
-  if (e.target.closest('.remove')) 
-  {
-     e.target.closest('.card').remove()
+  if (e.target.closest('.remove')) {
+    e.target.closest('.card').remove();
   }
-    
 });
