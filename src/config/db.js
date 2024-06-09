58e4eedf16c6e05a8b57df060070e73b0a265081
Aadithya-J <jlaadithya@gmail.com>
2024-06-09 20:40:04 +0530

    added error to check for .env

--- src/config/db.js
+++ src/config/db.js
@@ -1,9 +1,15 @@
 const mongoose = require('mongoose');
 
-const uri = process.env.MONGODB_URI;
-
 function connectDB(){
-  return mongoose.connect(uri);
+  const uri = process.env.MONGODB_URI;
+  if(uri){
+    return mongoose.connect(uri);
+  }
+  else{
+    return new Promise((resolve, reject) => {
+      reject('Error: MONGODB_URI is not set in .env file');
+    });
+  }
 }
 
 module.exports = {
